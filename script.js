ff5995bd9a8732dedcad7e20a338a35aa44a0f07
ChaosDeStack <mainawaweru018@gmail.com>
2026-07-28 10:32:23 +0100

    Add Akan name data table

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-
+// Akan names for each day of the week
+const akanNames = [
+  { day: "Sunday", male: "Kwasi", female: "Akosua" },
+  { day: "Monday", male: "Kwadwo", female: "Adwoa" },
+  { day: "Tuesday", male: "Kwabena", female: "Abenaa" },
+  { day: "Wednesday", male: "Kwaku", female: "Akua" },
+  { day: "Thursday", male: "Yaw", female: "Yaa" },
+  { day: "Friday", male: "Kofi", female: "Afua" },
+  { day: "Saturday", male: "Kwame", female: "Ama" }
+];
